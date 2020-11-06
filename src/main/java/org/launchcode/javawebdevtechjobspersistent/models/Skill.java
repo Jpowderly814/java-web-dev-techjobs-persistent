8c2c4386857fc93c418edba2459a1434d71635a5
Julie Larkin <julielarkin@Julies-MacBook-Air.local>
2020-11-06 10:38:39 -0800

    Finished part 2

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,32 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
+
+    @NotNull
+    @Size(min=3, max=200)
+    private String description;
+
+    public Skill() {
+    }
+
+    public Skill(String description) {
+        super();
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
 
 }
